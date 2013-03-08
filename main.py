729b40d62bdb588c41ff28984e25921cb187f645
nfredrik <nfredrik@localhost>
2013-03-08 19:15:22 +0100

    mail

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Mail import Mail
 def main(args):
     mail = Mail('fredrik.svard@gmail.com', 'frsv.linux@gmail.com', 'hoppa2lo', 'smtp.gmail.com')
     
-    mail.send('Sylvia Wrethammar')
+    mail.send('from pi', 'Sylvia Wrethammar')
 
  
 if __name__ == '__main__':
