311f86546e9ba8587969d8c521889d3f7cb1e269
nfredrik <nfredrik@localhost>
2013-03-10 13:03:28 +0100

    print name of datasource

--- collect.py
+++ collect.py
@@ -7,39 +7,69 @@ FILE='cobolmodules.pkl'
 def write_2_file(dict):
     
     output = open(FILE, 'wb')
+    # Save with new protocol i.e -1
     pickle.dump(dict, output, -1)
     output.close()
+    
+def from_file():
 
-filename = 'test.rrd'
-dataSources = []
-roundRobinArchives = []
-dataSource = DataSource(
-dsName='speed', dsType='COUNTER', heartbeat=600)
-dataSources.append(dataSource)
-roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=1, rows=24))
-roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=6, rows=10))
-myRRD = RRD(
-filename, ds=dataSources, rra=roundRobinArchives, start=920804400)
-myRRD.create()
+    if not os.path.exists(FILE):
+        return {}
 
-myRRD.bufferValue('920805600', '12363')
-myRRD.bufferValue('920805900', '12363')
-myRRD.bufferValue('920806200', '12373')
-myRRD.bufferValue('920806500', '12383')
-myRRD.update()
+    pkl_file = open(FILE, 'rb')
+    the_dict = pickle.load(pkl_file)
+    pkl_file.close()
+    return the_dict
 
-myRRD.bufferValue('920806800', '12393')
-myRRD.bufferValue('920807100', '12399')
-myRRD.bufferValue('920807400', '12405')
-myRRD.bufferValue('920807700', '12411')
-myRRD.bufferValue('920808000', '12415')
-myRRD.bufferValue('920808300', '12420')
-myRRD.bufferValue('920808600', '12422')
-myRRD.bufferValue('920808900', '12423')
-myRRD.update()
 
-write_2_file(dataSource)
+def main(args):
+    
+    filename = 'test.rrd'
+    
+    
+    if not os.path.exists(filename):
+        dataSources = []
+        roundRobinArchives = []
+        
+        dataSource = DataSource(dsName='speed', dsType='COUNTER', heartbeat=600)
+        print "dataSource.name:" , dataSource.name
+        dataSources.append(dataSource)
+        
+        roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=1, rows=24))
+        roundRobinArchives.append(RRA(cf='AVERAGE', xff=0.5, steps=6, rows=10))
+        
+        myRRD = RRD(filename, ds=dataSources, rra=roundRobinArchives, start=920804400)
+        myRRD.create()
+        
+        write_2_file(myRRD)
+    
+    else:
+        
+        myRRD = from_file()
+            
+        myRRD.bufferValue('920805600', '12363')
+        myRRD.bufferValue('920805900', '12363')
+        myRRD.bufferValue('920806200', '12373')
+        myRRD.bufferValue('920806500', '12383')
+        myRRD.update()
+        
+        myRRD.bufferValue('920806800', '12393')
+        myRRD.bufferValue('920807100', '12399')
+        myRRD.bufferValue('920807400', '12405')
+        myRRD.bufferValue('920807700', '12411')
+        myRRD.bufferValue('920808000', '12415')
+        myRRD.bufferValue('920808300', '12420')
+        myRRD.bufferValue('920808600', '12422')
+        myRRD.bufferValue('920808900', '12423')
+        myRRD.update()
+        
+        write_2_file(myRRD)
 
-print os.path.isfile(filename)
-print len(open(filename).read())
+        print os.path.isfile(filename)
+        print len(open(filename).read())
 
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+    
