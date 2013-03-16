e473394b1d0a36872cb9244b9a9b303f6424fb16
nfredrik <nfredrik@localhost>
2013-03-16 19:36:14 +0100

    Added ftp every hour

--- WebsiteMon/http.py
+++ WebsiteMon/http.py
@@ -2,6 +2,7 @@
 import rrd
 import time
 import httplib
+import logging
 from bahnhof import bahnHof
 
 # Todo: add logging feature, add mail everytime ftp is sent
@@ -13,11 +14,16 @@ use_ssl = False
 interval = 10
 rrd_file = 'test.rrd'
 
-report_interval = 60 * 60            
+report_interval = 60             
             
-def main():           
+def main(args):
+    
+    my_logger = args[0]
+               
     bahnhof = bahnHof()
+    
     report_time = time.time() + report_interval
+    my_logger.debug('Log initiated')
      
     my_rrd = rrd.RRD(rrd_file, 'Response Time')
     while True:   
@@ -29,9 +35,11 @@ def main():
             latency = ('%.3f' % raw_latency)
             my_rrd.update(latency)
             my_rrd.graph(60)
-            print latency
+            my_logger.debug('latency: %f'% latency)
+
         else:
             expire_time = interval
+            my_logger.debug('No response from site?')
         if expire_time > 0:
             time.sleep(expire_time)
                 
@@ -39,9 +47,13 @@ def main():
         if report_time < start_time:
             
             report_time = time.time() + report_interval
-            bahnhof.upload('index.html')
-            bahnhof.upload('test.rrd.png')  
-            
+            try:
+                bahnhof.upload('index.html')
+                bahnhof.upload('test.rrd.png')  
+                my_logger.debug('uploaded the suff')
+                my_logger.info('Uploaded info...')
+            except:
+                my_logger.debug('failed to upload the suff')              
            
 def send(host):
     if use_ssl:
@@ -53,15 +65,26 @@ def send(host):
         body = conn.getresponse().read()
         return True
     except:
-        print 'Failed request'
         return False
         
         
 if __name__ == '__main__':
+    
+    LOG_FILENAME = 'rrdlogger'
+    
+    my_logger = logging.getLogger('rrdLogger')
+    my_logger.setLevel(logging.DEBUG)
+                        
+    handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+                                               maxBytes=2048,
+                                               backupCount=5,
+                                               )
+    my_logger.addHandler(handler)
+
     try:
-        main()
+        main(my_logger)
     except:
-        print 'Exception'
+        my_logger.debug('exception!')
         
 
            
