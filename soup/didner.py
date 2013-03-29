1dc6f9deda8efe8442430e9c4d5198b2861de2b4
nfredrik <nfredrik@localhost>
2013-03-29 12:28:38 +0100

    didner

--- soup/didner.py
+++ soup/didner.py
@@ -4,6 +4,8 @@ url="http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000F01&progr
 page=urllib2.urlopen(url)
 #print page.read()
 soup = BeautifulSoup(page.read())
+print soup.prettify(encoding)
+
 universities=soup.findAll('table',{'class':'"alternatedtoplist halftoplist'})
 #universities=soup.findAll('a',class_='institution')
 #universities=soup.findAll('A',{'CLASS':'institution'})
