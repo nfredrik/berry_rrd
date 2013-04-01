7c2c72a3f6bc7bcb97f6e32d197cbf09ce902765
nfredrik <nfredrik@localhost>
2013-04-01 19:40:35 +0200

    First attempt to send mail about didner stuff

--- soup/didner.py
+++ soup/didner.py
@@ -1,58 +1,116 @@
 from BeautifulSoup import BeautifulSoup
+from Mail import Mail
+
 import urllib2
-
+import json
+import pickle
+import os
 # Constants
+FILE='yesterday.pickle'
 
-#OMX_20130329=375.35
-OMX_20130329=385.35
+OMX_20130329=375.35
 DIDNER_SVERIGE_20130229=1455.04
 RELATION_OMX_DIDNER_20130229=DIDNER_SVERIGE_20130229/OMX_20130329
 
-OMX_YESTERDAY=300.35
-DIDNER_SVERIGE_YESTERDAY=1450.04
+OMX_YESTERDAY=385.35
+DIDNER_SVERIGE_YESTERDAY=1455.04
 RELATION_OMX_DIDNER_YESTERDAY=DIDNER_SVERIGE_YESTERDAY/OMX_YESTERDAY
 
-sample = list()
+
+# Save values from today
+def write_2_file(dict):
+    
+    output = open(FILE, 'wb')
+    # Save with new protocol i.e -1
+    pickle.dump(dict, output, -1)
+    output.close()
+
+# Read values from yesterday
+def from_file():
+
+    if not os.path.exists(FILE):
+        return {}
+
+    pkl_file = open(FILE, 'rb')
+    the_dict = pickle.load(pkl_file)
+    pkl_file.close()
+    return the_dict
+
+obj = dict()
+
+if os.path.exists(FILE):
+    obj = from_file()
+else:
+    obj = {OMX_YESTERDAY:380.35, DIDNER_SVERIGE_YESTERDAY:1451.04}
+    write_2_file(obj)
+    
+    
+RELATION_OMX_DIDNER_YESTERDAY = obj[DIDNER_SVERIGE_YESTERDAY]/obj[OMX_YESTERDAY]
 
 dgstor = "http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000F01&programid=0000000000"
-# dgliten ="http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P0000IWH7&programid=0000000000"
+# dgliten ="0P0000IWH7"
 
-sample.append(dgstor)
-#sample.append(dgliten)
-#url="http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000F01&programid=0000000000"
 
-for d in sample:
-    page=urllib2.urlopen(d)
-    soup = BeautifulSoup(page.read())
-    #print soup.prettify()
-    didner=soup.find("table", {"class" : "alternatedtoplist halftoplist"})
-    #print didner
-    didner_kurs = didner.findAll('td')[1].text.replace(' ','').replace(',','.').replace('SEK','')
-    print type(didner_kurs)
-    didner_float = float(didner_kurs)
-    print didner_float
+sum= ""
+sum+='-'*50 +'\n'
+sum+="Didner yesterday:%.2f"%obj[DIDNER_SVERIGE_YESTERDAY]+'\n'
+sum+="OMX yesterday:%.2f"%obj[OMX_YESTERDAY]+'\n'
+
+
+
+#
+# Read OMX from text-TV
+#
 
 #  <span class="Y DH"> </span><span class="Y DH">OMX STOCKHOLM (13:00)   375.35  +0.67 </span>
 
 page=urllib2.urlopen("http://svt.se/svttext/web/pages/202.html")
 soup = BeautifulSoup(page.read())
-#print soup.prettify()
 OMX=soup.findAll("span", {"class" : "Y DH"})
 import re
 
-#print OMX[1].text
 
 filter = re.search(r"([\d]+\.[\d]+)", OMX[1].text) 
-
 if filter != None:
-    print 'OMX:', filter.group(0)
+    #sum+='OMX today:%s\n'% filter.group(0)
     omx_now = float(filter.group(0))
+
     
+
+
+
+#
+#  Read didner & gerge from morningstar
+#
+
+page=urllib2.urlopen(dgstor)
+soup = BeautifulSoup(page.read())
+#print soup.prettify()
+didner=soup.find("table", {"class" : "alternatedtoplist halftoplist"})
+didner_float = float(didner.findAll('td')[1].text.replace(' ','').replace(',','.').replace('SEK',''))
+
+sum+= 'Didner compared to OMX from yesterday:%.2f'%(((didner_float/omx_now/RELATION_OMX_DIDNER_YESTERDAY)-1)*100)+'%\n'
+
+sum+='-'*50 +'\n'
+sum+="Didner course today: %.2f\n"% didner_float
+sum+="OMX today: %.2f\n"% omx_now    
+#print 'Difference from 2013039: %.2f'%(((didner_float/omx_now/RELATION_OMX_DIDNER_20130229)-1)*100),'%'
+sum+='Didner compared to OMX from 20130329: %.2f'%(((didner_float/omx_now/RELATION_OMX_DIDNER_20130229)-1)*100) + '%\n'
+
+
+
+
+
+
+obj[OMX_YESTERDAY]=omx_now
+obj[DIDNER_SVERIGE_YESTERDAY] =didner_float
+
+write_2_file(obj)
+
+
+#print '---Printing sum---'
+print sum
+
+mail = Mail('fredrik.svard@gmail.com', 'frsv.linux@gmail.com', 'hoppa2lo', 'smtp.gmail.com')
     
-print 'Difference from 2013039:%.2f'%(((didner_float/omx_now/RELATION_OMX_DIDNER_20130229)-1)*100),'%'
-
-
-print 'Difference from yesterday:%.2f'%(((didner_float/omx_now/RELATION_OMX_DIDNER_YESTERDAY)-1)*100),'%'
-
-
-# 
+mail.send('Didner och OMX', sum,[])
