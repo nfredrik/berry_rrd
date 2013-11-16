27dd16a68749349f9d22e58ebb0e36134e3d2c7a
nfredrik <nfredrik@localhost>
2013-11-16 16:39:05 +0100

    better

--- generate_load/gen_load.py
+++ generate_load/gen_load.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python -tt
 
-
+import os
 import sys
 import time
+import shutil
 
 def main(args):
 
-
+    try:
+        os.remove('test.txt')
+        os.remove('test2.txt')
+    except:
+        pass
+    
     # Create file with random data
     nisse = open('test.txt', 'w')
     for i in range(10000):
@@ -15,14 +21,19 @@ def main(args):
     nisse.close()
     
     # Do some I/O-work for a while
-    max_time =5
+    max_time =60
     start_time = time.time()  # remember when we started
     while (time.time() - start_time) < max_time:
+        nisse2 = open('test2.txt', 'w')
         nisse = open('test.txt', 'r')
         for row in nisse.read():
-            print row
+            # print row
+            nisse2.write('hello')
+            
+   
        
         nisse.close()
+        nisse2.close()
     
     
     print 'the end'
