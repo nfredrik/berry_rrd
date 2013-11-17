0c1757325a280978d09fbf1ed9f20d6fdbe1ff68
Fredrik ard <nfredrik@hotmail.com>
2013-11-17 22:52:32 +0100

    included dd

--- generate_load/jot.sh
+++ generate_load/jot.sh
@@ -1,6 +1,11 @@
 #!/bin/sh 
 
-for counter in $(jot 10)
+rm -fr file_100MB
+
+dd if=/dev/zero bs=1024 count=100000 of=file_100MB
+
+for counter in $(jot 500)
 do
-	echo $counter
+        dd if=file_100MB of=/dev/null bs=64k
+	#echo $counter
 done
